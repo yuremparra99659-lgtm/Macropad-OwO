7b77f5c1f58da0b6d1cd6ff84ab0884b1b65504b
Yuyo <yuremparra99659@gmail.com>
2026-02-05 17:45:12 -0600

    Add files via upload

---
+++ qmk firmware/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 1
+
+#define DEBOUNCE 5
---
+++ qmk firmware/keymaps/default/keymap.c
@@ -0,0 +1,14 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_Q,
+        KC_W,
+        KC_E,
+        KC_U,
+        KC_I,
+        KC_O
+    )
+
+};
